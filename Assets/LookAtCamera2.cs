0ca07e5fc35809f5a90b2797777c5d25d1516631
Mads Home <bockchristensen@gmail.com>
2017-01-20 19:11:20 +0100

    Mads' attempt at a system. The red box has the new system, the white the old for comparison. The big idea is that the health bars forward vector should be the opporsite of the camera's forward vector

---
+++ Assets/LookAtCamera2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Camera cam = Camera.main;
+        transform.rotation = Quaternion.LookRotation(-cam.transform.forward, cam.transform.up);
+	}
+}
